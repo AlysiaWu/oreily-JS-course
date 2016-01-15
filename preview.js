a87fc7389c5442cfd47d4b8c8d9b569353328366
Alysia <wwu@springpathinc.com>
2016-01-15 15:17:22 -0800

    canvas

--- preview.js
+++ preview.js
@@ -1,18 +1,40 @@
-(function Preview (){
+(function Preview (window){
 	this.init = function () {
-		var aLinks = document.getElementByTagName("a");
+		var aLinks = document.getElementsByTagName("a");
+		console.log(aLinks);
 		for (var i= 0; i < aLinks.length; i++) {
-			aLinks[i].onClick = preview; 
+			aLinks[i].onclick = this.preview;
+
 		}
+		var close = document.getElementsByClassName("close")[0];
+		console.log(close);
+		close.onclick = this.close;
 	}
-	this.preview = function () {
-		preventDefault();
-		// conso
-		var div = document.createElement("div");
-		var preImg = document.createElement("img");
-		preImg.setAttribute("src", 'https://courses.oreillyschool.com/advancedjavascript/homework/images/image1.jpg');
-		div.appendChild(preImg);
+	this.preview = function (e) {
+		console.log("onclick");
+		e.preventDefault();
+		var preview = e.target.getAttribute("data-preview");
+		var el = document.querySelectorAll("[data-preview=" + preview + "]")[0];
+		
+		var imageSrc = el.getAttribute("href");
+
+		var imageEl = document.getElementById("previewImage");
+		imageEl.setAttribute("src", imageSrc);
+		var containerEl = document.getElementById("container");
+		containerEl.className = "showOverlay";
 	}
 
-})()
+	this.close = function(e) {
+		console.log("close function");
+		var containerEl = document.getElementById("container");
+		containerEl.className = "";
+
+
+	}
+	// this.appendImage() = function() {
+
+	// }
+	window.Preview = this;
+
+})(window)
 
